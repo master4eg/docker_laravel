6965d37508c08a8ec00e607c303651ec550f6050
Stadnyuk Vitaly <stadnyuk_vitaly@mail.ru>
2025-09-22 15:52:48 +0500

    Update mysql config aka Laravel default config

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,1 @@
--- Настройка по умолчанию, можно удалить файл, если не нужно
-CREATE DATABASE IF NOT EXISTS `app` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS 'app'@'%' IDENTIFIED BY 'secret';
-GRANT ALL PRIVILEGES ON `app`.* TO 'app'@'%';
-FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS `laravel` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
